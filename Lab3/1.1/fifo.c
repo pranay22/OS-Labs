fe9c095c1d2b7df9ed9bfa8f3e7252fba128b5f8
Saad Ouassil Allak <sw.allak@gmail.com>
2016-01-24 14:25:02 +0100

    Task 1.1 Lab3 init & Makefile

--- Lab3/1.1/fifo.c
+++ Lab3/1.1/fifo.c
@@ -16,37 +16,7 @@ struct data_item {
 	unsigned long long time;//timestamp (sec) for item creation
 	char *msg;		//NULL terminated C string
 }
-//Initialization of FIFO buffer
-if(!arg[0]) {
-	struct data_item buffer[BUFFER_SIZE];
-}
-else {
-	struct data_item buffer[arg[0]];
-}
-//For race condition handling
-static struct semaphore full;
-static struct semaphore empty;
-static struct semaphore cr;	/* Mutex for critical region */
 
-
-//device operations 
-// - kind of confused what to add and what not to...
-
-
-//Initialization - for 'insmod'
-static int __init fifo_init(void) {
-	printk(KERN_INFO "FIFO device loaded\n");
-	//Initialize semaphores
-	sema_init(&full, 0);
-	sema_init(&empty, BUFFER_SIZE);
-	sema_init(&cr, 1); //Using this as a mutex
-	return 0;
-}
-
-//Cleanup - for 'rmmod'
-static void __exit fifo_cleanup(void) {
-	printk(KERN_INFO "FIFO device unloaded\n");
-}
 
 
 module_init(fifo_init);
