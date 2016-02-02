67ab2f412bcf6673798556b8deefe293e0fb316e
Saad Ouassil Allak <sw.allak@gmail.com>
2016-02-02 22:59:26 +0100

    task 1.3

--- Lab3/1.3/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-#$1 module's path
-#$2 modole's buffer size
-
-from subprocess import call;
-call(["ls"]);
--- Lab3/1.3/test.sh
+++ Lab3/1.3/test.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 ## Test script, please execute with root privileges. We promise we won't harm your system.
 ## At least not deliberately... ;)
-
+#set -x
+echo
 echo 'Installing kernel module'
-cd ../1.1
-make
-insmod fifo.ko major=240
-mknod -mode=666 /dev/deeds_fifo c 240 1
-cd ../1.3
+cd ../1.2
 echo 'Kernel module installed'
-echo 'Filling fifo with 32 items'
-echo –n "0,1451001600,Merry Christmas!" > /dev/deeds_fifo
+echo 'Trying simple read and write'
+echo "0,1,Merry Christmas!" > /dev/deeds_fifo
 cat /dev/deeds_fifo
+(
+	echo 'Reading form an empty FIFO'
+	echo 'This will blockk'
+	cat /dev/deeds_fifo &
+	cat /dev/deeds_fifo &
+	cat /proc/deeds_fifo_stats
+)
+(
+	sleep 5
+	echo 'Deblocking'
+	echo '1,1,Thank me for letting you out' > /dev/deeds_fifo
+	sleep 5
+	echo '1,2,Thank me for letting you out' > /dev/deeds_fifo 
+)
+(
+	echo 'Filling in the FIFO'
+    COUNTER=0
+   	while [  $COUNTER -lt 32 ]; do
+		echo "0,1,Message $COUNTER" > /dev/deeds_fifo
+        let COUNTER=COUNTER+1 
+   	done
+	echo "Current statistics"
+	cat /proc/deeds_fifo_stats
+)
+(
+	echo "Writing to a full FIFO (Blocking)"
+	echo 
+)
+
